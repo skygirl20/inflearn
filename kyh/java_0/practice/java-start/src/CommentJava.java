b8fb18c954e2cd907f0ec3539d40060a562fb040
박민서 <parkmin100@neighbor21.co.kr>
2025-06-17 14:42:58 +0900

    김영한 로드맵 java_0 시작

---
+++ kyh/java_0/practice/java-start/src/CommentJava.java
@@ -0,0 +1,9 @@
+public class CommentJava {
+    public static void main(String[] args) {
+        System.out.println("Hello java1"); // hello java1을 출력합니다.
+//        //System.out.println("Hello java2");
+//        System.out.println("Hello java3");
+//        System.out.println("Hello java3");
+
+    }
+}
